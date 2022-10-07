5c6c429055e231182ed181d3f778ffbf3aa2c37e
erika-h7 <erika.herrera1@outlook.com>
2022-10-07 14:48:53 -0700

    Used UIKit Components with SwiftUI, created a reusable WebView Component

--- H4X0R News/Views/ContentView.swift
+++ H4X0R News/Views/ContentView.swift
@@ -12,18 +12,33 @@ struct ContentView: View {
     @ObservedObject var networkManager = NetworkManager()
     
     var body: some View {
+        
         NavigationView {
+            
             List(networkManager.posts) { posts in
-                HStack {
-                    Text(String(posts.points))
-                        .foregroundColor(Color(UIColor(red: 0.47, green: 0.27, blue: 0.89, alpha: 1.00)))
-                    Text(posts.title)
+                NavigationLink(destination: DetailView(url: posts.url)) {
+                    HStack {
+                        Text(String(posts.points))
+                            .foregroundColor(Color(UIColor(red: 0.47, green: 0.27, blue: 0.89, alpha: 1.00)))
+                            .padding(.horizontal)
+                        Text(posts.title)
+                        Spacer()
+                    }
+                    
                 }
+                
+                .foregroundColor(Color(UIColor(red: 0.54, green: 0.75, blue: 0.11, alpha: 1.00)))
+                .listRowBackground(Color(UIColor(red: 0.07, green: 0.03, blue: 0.17, alpha: 1.00)))
+                .frame(maxWidth: .infinity)
+                .padding(.vertical)
+                
             }
             .navigationBarTitle("H4X0R NEWS")
             .toolbarColorScheme(.light, for: .navigationBar)
             .toolbarBackground(Color(UIColor(red: 0.54, green: 0.75, blue: 0.11, alpha: 1.00)), for: .navigationBar)
             .toolbarBackground(.visible, for: .navigationBar)
+            .padding(.vertical)
+            
         }
         .onAppear {
             self.networkManager.fetchData()
---
+++ H4X0R News/Views/DetailView.swift
@@ -0,0 +1,28 @@
+//
+//  DetailView.swift
+//  H4X0R News
+//
+//  Created by Infinity Code on 10/7/22.
+//
+
+import SwiftUI
+
+
+struct DetailView: View {
+    
+    let url: String?
+    
+    var body: some View {
+        WebView(urlString: url)
+    }
+}
+
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "https://www.google.com")
+    }
+}
+
+
+
